2f420e73acec7ae80115142421940d884ae31dcc
gopichandubolisetty <gopichandubolisetty@gmail.com>
2026-06-28 14:36:03 +0530

    Updation of the department

--- src/main/java/com/example/employee_management_api/service/EmployeeService.java
+++ src/main/java/com/example/employee_management_api/service/EmployeeService.java
@@ -13,7 +13,7 @@ public class EmployeeService {
     private List<Employee> employees = new ArrayList<>();
 
     public EmployeeService() {
-        employees.add(new Employee(1, "Senthur", "DevOps", 150000));
+        employees.add(new Employee(1, "Senthur", "DevOps and GenAi", 150000));
         employees.add(new Employee(2, "John", "Testing", 145000));
     }
 
